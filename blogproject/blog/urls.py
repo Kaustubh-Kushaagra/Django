829fdace8b385f489c9c38a4e0fdbe4a761f1865
Kaustubh Kushaagra <kaustubhkushaagra@gmail.com>
2026-03-20 19:45:16 +0530

    Initial commit: Django projects

--- blogproject/blog/urls.py
+++ blogproject/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from blog.views import post_list_view,post_detail
+from blog.views import post_list_view,post_detail,PostListView
 app_name='blog'
 urlpatterns = [
-    path('',post_list_view,name="post_list_view"),
+    # path('',post_list_view,name="post_list_view"),
     path('<int:id>/', post_detail, name='post_detail'),
+    path('', PostListView.as_view(), name='post_list'),
 ]
--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,8 +1,47 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from django.views.generic import ListView
+from blog.forms import EmailPostForm
 # Create your views here.
+def post_share(request, post_id):
+    post = get_object_or_404(
+    Post,
+    id=post_id,
+    status=Post.Status.PUBLISHED
+    )
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+        return render(
+        request,
+        'blog/share.html',
+        {
+        'post': post,
+        'form': form
+        }
+    )
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post_list.html'
+
 def post_list_view(request):
     posts=Post.published.all()
+    post_list_view=Post.published.all()
+    paginator = Paginator(post_list_view, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request,"blog/post_list.html",{"posts":posts})
 def post_detail(request, id):
     post = get_object_or_404(
